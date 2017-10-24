87ec193ade194eefe26064b25df0364daa8a7be2
pdzsbl <2823694360@qq.com>
2017-10-25 00:02:00 +0800

    alpha

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# crawler
+#Crawler
+-------
+> Crawler is a small web crawler for pixiv, based on python3.5.2
+
+## Features
+- Find pictures under specific keyword searching(the default keyword is lovelive) of which the number of stars is larger than a specific number(the default number is 200)
+- Support turning to next page and find other pictures.The defalt page range is 20 to 40
+
+## Usage
+This crawler depends on re, urllib and selenium. You can just use pip or easy install or even just install package by pycharm to install these libs.
+
+If you have installed all above libs, you can use this crawler just by entering
+
+'''
+python crawler.py
+'''
+
+After u start, u need to enter ur pixiv account and pw by urself. And u wiil get all the picture url and their star number in a txt.
--- crawler.py
+++ crawler.py
@@ -1,30 +1,66 @@
+#encoding:utf-8
+from time import sleep
+
 import re
 import urllib.request
 import urllib
 
-from collections import deque
+from selenium import webdriver
 
-picture = deque()
-visited_page = set()
-visited_pictures = set();
+picture_star=[]
+picture = []
 
+driver = webdriver.Chrome("C:\\Users\hasee\AppData\Local\Programs\Python\Python35\Scripts\chromedriver.exe")
+driver.get("https://accounts.pixiv.net/login?lang=zh&source=pc&view_type=page&ref=wwwtop_accounts_index")
+#放弃让爬虫自己登陆，改为手动登陆后等待爬虫帮我爬网页
+print("start now")
+sleep(5)#这里根据网络连接状态自己决定停多少秒，反正如果卡升天的话等待加载的时间基本是允许你输入加登录的，所以也可以去掉这一行
 
-data={}
-data['word']=''#此处输入搜索关键词
+search_key_word='lovelive'#此处输入搜索关键词
 
-url_values = urllib.parse.urlencode(data)
 url_former = "https://www.pixiv.net/search.php"
 url_former0 = "?s_mode=s_tag&word="
-nextpage = url_former + url_former0 + url_values
-pagenumber = 1
-while nextpage:#nextpage不为空就一直继续
-    visited_page|={nextpage}
-    pagenumber = pagenumber + 1
-    next_page_end = "?word=adf&order=date_d&p="+pagenumber
-    nextpage = nextpage 
+nextpage = url_former + url_former0 + search_key_word
+pagenumber = 20#开始的页数
+last_page = 40#结束的页数
+
+save_path = 'D:\\test'+str(pagenumber)+'-'+str(last_page)+'.txt'
+f_obj = open(save_path, 'w', encoding="utf-8")
+f_obj.write('\n')
+while pagenumber<=last_page:
+
+
+    next_page_end = "?word="+search_key_word+"&order=date_d&p="+str(pagenumber)
+    nextpage = url_former+next_page_end
+
     try:
-        urlop = urllib.request.urlopen(nextpage,timeout = 10)
+        driver.get(nextpage)
     except:
         continue
 
+    try:
+        page_content = driver.page_source
+    except:
+        continue
+    #读取所有的图片地址到一个集合，然后读取所有的star数目到一个集合
+    pic_pattern = re.compile('href="/member_illust.php\?mode=medium&amp;illust_id=\d+?".+?</figcaption>')#寻找全部图片
+    star_pic_pattern = re.compile('href="(/member_illust.php\?mode=medium&amp;illust_id=\d+?)".+?bookmark-badge"></i>.+?</a>')#寻找有star的图片和star数目
+    star_pattern = re.compile('bookmark-badge"></i>(.+?)</a>')
+    for y in star_pattern.findall(page_content):
+        picture_star.append(int(y))
+    print("now it's scanning page "+str(pagenumber))
+    picturenumber = 0
 
+    for x in pic_pattern.findall(page_content):
+        l = star_pattern.findall(x)
+        #如果l非空，意味着有star，进行判断
+        if l:
+            if int(l[0])>200:
+                k = star_pic_pattern.findall(x)
+                f_obj.write("www.pixiv.net"+k[0]+" "+l[0]+"\n")
+
+    picture_star.clear()
+    picture.clear()
+    pagenumber = pagenumber+1
+print(over)
+f_obj.close()
